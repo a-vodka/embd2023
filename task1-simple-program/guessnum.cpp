8b2d30ca5ba758b2144b35f983d56aaa44e026f5
zaitt4 <zaitt4@gmail.com>
2023-11-14 02:55:17 +0200

    changes to guessnum and gitignore
    guessnum.cpp: add body
    .gitignore: ignores *.out files

--- task1-simple-program/guessnum.cpp
+++ task1-simple-program/guessnum.cpp
@@ -1,8 +1,14 @@
-#include <iostream>
+#include <iostream>     // printf, cin
+#include <time.h>       // time
 
 using namespace std;
 
+const int MAX = 30000;	//cin buffer clean limit
+
 int main(){
+
+	printf("Guess number game. Try to guess my number in range [0;9]\n");
+	srand(time(0));				//plant random seed
 
 	return 0;
 }

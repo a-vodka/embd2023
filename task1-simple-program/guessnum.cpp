35f01533576059bff755db4658dab1f2e9c9e275
zaitt4 <zaitt4@gmail.com>
2023-11-14 02:59:23 +0200

    guessnum add loop
    guessnum.cpp: add loop (game itself)

--- task1-simple-program/guessnum.cpp
+++ task1-simple-program/guessnum.cpp
@@ -8,7 +8,37 @@ const int MAX = 30000;	//cin buffer clean limit
 int main(){
 
 	printf("Guess number game. Try to guess my number in range [0;9]\n");
-	srand(time(0));				//plant random seed
+	srand(time(0));		//plant random seed
 
-	return 0;
+	int user,					//var for user input
+        computer = rand() % 9;	//random number
+
+	while(1){
+        try {
+            printf("input >> ");	//prompt
+            cin >> user;
+            if      ( cin.fail()  )                  throw 1;	//wrong input (not number)
+            else if ( (0 > user) || (9 < user) )     throw 2;	//input out of range [0;9]
+            else if ( user != computer )             throw 3;	//lose condition
+            else if ( user == computer )             throw 4;	//win condition
+        }
+        catch(int e){
+
+            if ( (e == 1) || (e == 2) ) {
+                cin.clear();
+                cin.ignore(MAX, '\n');							//clean cin buffer
+                printf("Enter a number in range [0;9]\n");		//prompt
+            }
+            else if (e == 3) {
+                printf("You lose! My number was %d\n", computer);
+                return 1;	//lose
+            }
+            else if (e == 4) {
+                printf("Wow, you win, you're so smart\n");		//tease
+                return 0;	//win
+            }
+        }
+    }
+
+	return 2;	//if while loop did not work
 }

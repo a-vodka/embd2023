907a735ee304643aa4704a105476d64c83ead1e1
HappyNext <happyoleg69@gmail.com>
2023-11-23 11:54:44 +0200

    Task1: add Check function

--- task1.cpp
+++ task1.cpp
@@ -3,8 +3,26 @@
 
 using namespace std;
 
+int Check (int comp_value, int value)
+{
+    if (comp_value == value)
+    {
+	cout << "You win! ^-^" << endl;
+	return 0;
+    }
+    else
+    {
+	cout << "You lose! т_т" << endl;
+	return 1;
+    }
+}
+
 int main()
 {
     int comp_value = rand() % 10;
-    return 0;
+    int value;
+    cout << "Введіть число: " << endl;
+    cin >> value;
+    int answer = Check(comp_value, value);
+    return answer;
 }

b7e4d2a95b6c8f21db392adba391581add6b8f26
HappyNext <happyoleg69@gmail.com>
2023-11-30 17:12:25 +0200

    Task1: Add Check function and complete

--- task1-simple-program/task1.cpp
+++ task1-simple-program/task1.cpp
@@ -3,11 +3,26 @@
 
 using namespace std;
 
+int Check (int comp_value, int value)
+{
+    if (comp_value == value)
+    {
+	cout << "You win! ^-^" << endl;
+	return 0;
+    }
+    else
+    {
+	cout << "You lose! т_т" << endl;
+	return 1;
+    }
+}
+
 int main()
 {
     int comp_value = rand() % 10;
     int value;
     cout << "Введіть число: ";
     cin >> value;
-    return 0;
+    int answer = Check(comp_value, value);
+    return answer;
 }

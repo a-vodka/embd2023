c91faecd34bc811a55c3246d5f07dc3c7814c4b9
HappyNext <happyoleg69@gmail.com>
2023-11-30 17:10:13 +0200

    Task1: Add comp and user values

--- task1-simple-program/task1.cpp
+++ task1-simple-program/task1.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <random>
 
 using namespace std;
 
 int main()
 {
+    int comp_value = rand() % 10;
+    int value;
+    cout << "Введіть число: ";
+    cin >> value;
     return 0;
 }
